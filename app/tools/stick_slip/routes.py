6f8703ea9a8eedbcdfafdd11dd18eabda872764a
Mike Lloyd <49411532+mike-lloyd03@users.noreply.github.com>
2020-08-06 17:18:47 -0700

    Update routes.py
    Remove os.remove() calls

--- app/tools/stick_slip/routes.py
+++ app/tools/stick_slip/routes.py
@@ -11,9 +11,9 @@ from .static.filterTDMS import filter_and_interpolate_data
 @bp.route('/')
 def stick_slip_upload_form():
     form = UploadForm()
-    static_folder = os.path.join(bp.static_folder, 'temp')
-    for file in os.listdir(static_folder): 
-        os.remove(static_folder + '/' + file)
+#    static_folder = os.path.join(bp.static_folder, 'temp')
+#    for file in os.listdir(static_folder): 
+#        os.remove(static_folder + '/' + file)
     return render_template('upload.html', form=form)
 
 @bp.route('/', methods=['GET', 'POST'])
@@ -25,7 +25,7 @@ def stick_slip_results_view():
         uploaded_file.save(filename)
         output_img = filter_and_interpolate_data(filename)
         print(output_img)
-        os.remove(filename)
+#        os.remove(filename)
     else:
         return redirect(url_for('stick_slip.stick_slip_upload_form'))
     return render_template('results_view.html', output_img=output_img)
